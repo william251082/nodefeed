974431a021d8d6f5e80ee8b03284e996f7f8ba32
william <william.del.rosario@iceshop.nl>
2020-08-14 23:58:39 +0200

    Defining a Mutation Schema

--- feeds/src/graphql/schema.ts
+++ feeds/src/graphql/schema.ts
@@ -1,14 +1,35 @@
 import {buildSchema} from "graphql";
 
 export default buildSchema(`
-    type TestData {
-        text: String!
-        views: Int!
+    type Post {
+        _id: ID!
+        title: String!
+        content: String!
+        imageUrl: String!
+        creator: User!
+        createdAt: String!
+        updatedAt: String!
     }
-    type RootQuery {
-        hello: String
+
+    type User {
+        _id: ID!
+        name: String!
+        email: String!
+        password: String
+        status: String!
+        posts: [Post!]!
+    }
+    
+    input UserInputData {
+        email: String!
+        name: String!
+        password: String!
+    }
+    type RootMutation {
+        createUser(userInput: UserInputData): User!
     }
     schema {
         query: RootQuery
+        mutation: RootMutation
     }    
 `);
