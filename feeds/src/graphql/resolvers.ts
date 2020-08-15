4edffffbbae88e0be884d6aa6ff716b5445a0ed0
william <william.del.rosario@iceshop.nl>
2020-08-15 10:51:47 +0200

    Updating Posts

--- feeds/src/graphql/resolvers.ts
+++ feeds/src/graphql/resolvers.ts
@@ -165,4 +165,57 @@ export default {
       updatedAt: post_doc.updatedAt.toISOString()
     };
   },
+    updatePost: async function({ id, postInput }: any, req: IObjectExtend) {
+    if (!req.isAuth) {
+      const error: IObjectExtend = new Error('Not authenticated!');
+      error.code = 401;
+      throw error;
+    }
+    const post = await Feed.findById(id).populate('creator');
+    if (!post) {
+      const error: IObjectExtend = new Error('No post found!');
+      error.code = 404;
+      throw error;
+    }
+    let feed: any;
+    feed = post;
+    if (feed.creator._id.toString() !== req.userId.toString()) {
+      const error: IObjectExtend = new Error('Not authorized!');
+      error.code = 403;
+      throw error;
+    }
+    const errors = [];
+    if (
+      validator.isEmpty(postInput.title) ||
+      !validator.isLength(postInput.title, { min: 5 })
+    ) {
+      errors.push({ message: 'Title is invalid.' });
+    }
+    if (
+      validator.isEmpty(postInput.content) ||
+      !validator.isLength(postInput.content, { min: 5 })
+    ) {
+      errors.push({ message: 'Content is invalid.' });
+    }
+    if (errors.length > 0) {
+      const error: IObjectExtend = new Error('Invalid input.');
+      error.data = errors;
+      error.code = 422;
+      throw error;
+    }
+    post.title = postInput.title;
+    post.content = postInput.content;
+    if (postInput.imageUrl !== 'undefined') {
+      post.imageUrl = postInput.imageUrl;
+    }
+    const updatedPost = await post.save();
+        let updated_post: any;
+    updated_post = updatedPost;
+    return {
+      ...updatedPost,
+      _id: updatedPost._id.toString(),
+      createdAt: updated_post.createdAt.toISOString(),
+      updatedAt: updated_post.updatedAt.toISOString()
+    };
+  },
 };
